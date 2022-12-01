fb1863068380e1c4b25bcab80a75fa20216a529a
alras63 <aleksey171002@gmail.com>
2022-12-01 22:56:36 +0400

    hostfix

--- config/telebot.php
+++ config/telebot.php
@@ -28,7 +28,7 @@ return [
             'name'       => env('TELEGRAM_BOT_NAME', null),
             'api_url'    => env('TELEGRAM_API_URL', 'https://api.telegram.org/bot{TOKEN}/{METHOD}'),
             'exceptions' => true,
-            'async'      => true,
+            'async'      => false,
 
             'webhook' => [
                 'url'               => env('TELEGRAM_BOT_WEBHOOK_URL', env('APP_URL').'/telebot/webhook/bot/'.env('TELEGRAM_BOT_TOKEN')),
