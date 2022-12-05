32c6545cd4a2e7656b788f7d992a2eb03e9baa0f
alras63 <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>
2022-12-05 08:44:39 +0400

    edit

--- resources/views/checkQR.blade.php
+++ resources/views/checkQR.blade.php
@@ -2,7 +2,7 @@
     <img style="width: 100%" src="{{ \Illuminate\Support\Facades\Storage::url('/error.png') }}" alt="ошибка">
 @elseif($bool === true && isset($priemRequest))
     <img style="width: 100%" src="{{  \Illuminate\Support\Facades\Storage::url('/true.png') }}" alt="подтверждено">
-    <h2>
+    <h2 style="font-size: 40px">
         {{isset($priemRequest->tguser) ? "Проверьте ФИО в паспорте!: " . $priemRequest->tguser->fio : "ФИО не указано"}}
     </h2>
 @endif
